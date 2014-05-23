99c630760a32479e2294ce49cf5d352a25b70205
thoyt <thoyt@berkeley.edu>
2014-05-24 00:02:45 +0100

    Deployment

--- right_now/right_now/wsgi.py
+++ right_now/right_now/wsgi.py
@@ -8,7 +8,21 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "right_now.settings")
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/opt/right_now/venv/lib/python2.6/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/opt/right_now/right_now')
+sys.path.append('/opt/right_now/right_now/survey')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'right_now.settings'
+
+# Activate the virtual env
+activate_env=os.path.expanduser("/opt/right_now/venv/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
